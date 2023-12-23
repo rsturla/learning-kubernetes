f45832657deac423b528fe1e844c41065e75a22a
Robert Sturla <robertsturla@outlook.com>
2023-12-23 19:46:55 +0000

    Script to setup a local Kubernetes cluster with Kind

---
+++ bootstrap/setup-local.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Function to check if a command or program exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Function to check for required commands
+check_required_commands() {
+    # Define required commands
+    local required_commands=("kind" "kubectl")
+
+    # Array to store missing commands
+    local missing_commands=()
+
+    # Check if the required commands are available
+    for cmd in "${required_commands[@]}"; do
+        if ! command_exists "$cmd"; then
+            missing_commands+=("$cmd")
+        fi
+    done
+
+    # Output the list of missing commands and exit if any are missing
+    if [ "${#missing_commands[@]}" -gt 0 ]; then
+        echo "The following tools must be installed:"
+        printf -- '- %s\n' "${missing_commands[@]}"
+        exit 1
+    fi
+
+    echo "All required tools are installed."
+}
+
+# Function to create the cluster.  The name of the cluster is passed as an argument
+create_cluster() {
+    # Create the cluster
+    kind create cluster --name "$1" --config ./config/kind-config.yaml
+}
+
+# Call the function to check for required commands
+check_required_commands
+
+# Create the cluster
+create_cluster "local-cluster-1"
